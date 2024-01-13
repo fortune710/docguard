553ff82c43a85ccc2e20d448e656450296a57af2
fortune710 <fortune0208@yahoo.com>
2024-01-13 17:14:11 -0500

    added password hashing

--- server/users/createUser.ts
+++ server/users/createUser.ts
@@ -1,11 +1,18 @@
 import { prisma } from "@/prisma"
+import hashPassword from "./hashPassword"
 
 const createUser = async (data: any) => {
+    const hashedPassword = hashPassword(data.password);
+
     const newUser = await prisma.user.create({
-        data,
+        data: {
+            ...data,
+            image: `https://api.dicebear.com/7.x/adventurer/svg?seed=${data.name}`,
+            password: hashedPassword
+        },
         select: {
             id: true,
-            full_name: true,
+            name: true,
             email: true
         }
     })
