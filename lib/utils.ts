acf8890f190f317c1de41f64b950626e7cc3604d
fortune710 <fortune0208@yahoo.com>
2024-02-02 00:45:19 -0500

    added urbanist font

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Urbanist } from "next/font/google";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const urbanist = Urbanist({
+  subsets: ["latin"],
+  variable: '--font-urbanist'
+})
+
+
