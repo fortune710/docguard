5bb4219741b8a70680d84a7eefedbeb964b69978
fortune710 <fortune0208@yahoo.com>
2024-02-15 00:53:22 -0500

    create document action

--- app/actions.ts
+++ app/actions.ts
@@ -1,16 +1,15 @@
 'use server'
 
-import uploadFile from "@/services/s3/uploadFile";
 import getFile from "@/services/s3/getFile";
 import createNewDocument from "@/server/documents/createNewDocument";
-import { prisma } from "@/prisma";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const addNewDocumentAction = async (userId: string, formData: FormData) => {
     const file = formData.get("file") as File;
     const formEntries = formData.entries();
-    const { title, description, expiry_date, user_id } = Object.fromEntries(formEntries);
+    const { title, description, expiry_date, category } = Object.fromEntries(formEntries);
 
     const url = await getFile(file.name);
 
@@ -19,11 +18,13 @@ export const addNewDocumentAction = async (userId: string, formData: FormData) =
         title: title.toString(),
         description: description.toString(),
         url,
-        expiry_date: expiry_date || null,
-        user_id: userId
+        expiry_date: new Date(expiry_date as any).toISOString() || null,
+        user_id: userId,
+        category
     })
     
     revalidatePath('/')
+    redirect('/home')
 }
 
 
