c329c2a9c77f47068b7dab67d67962790993f480
fortune710 <fortune0208@yahoo.com>
2024-02-18 21:52:58 -0500

    stored file buffer in s3

--- app/actions.ts
+++ app/actions.ts
@@ -5,22 +5,28 @@ import createNewDocument from "@/server/documents/createNewDocument";
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import uploadFile from "@/services/s3/uploadFile";
 
 export const addNewDocumentAction = async (userId: string, formData: FormData) => {
     const file = formData.get("file") as File;
     const formEntries = formData.entries();
     const { title, description, expiry_date, category } = Object.fromEntries(formEntries);
 
-    const url = await getFile(file.name);
+    const expiryDate = !expiry_date ? null : new Date(expiry_date as any).toISOString();    
+    //const url = await getFile(file.name);
 
+    const fileBuffer = await file.arrayBuffer();
+
+    
+    await uploadFile(file.name!, fileBuffer)
     
     await createNewDocument({
         title: title.toString(),
         description: description.toString(),
-        url,
-        expiry_date: new Date(expiry_date as any).toISOString() || null,
+        file_key: file.name,
+        expiry_date: expiryDate,
         user_id: userId,
-        category
+        category,
     })
     
     revalidatePath('/')
