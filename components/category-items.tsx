eae00c02a77df4d6c86089c33906b521af08508f
fortune710 <fortune0208@yahoo.com>
2024-02-18 22:04:53 -0500

    used category definitions from utils

--- components/category-items.tsx
+++ components/category-items.tsx
@@ -1,24 +1,8 @@
 'use client'
 
-import { FiUser } from "react-icons/fi";
-import { FaMoneyBill } from "react-icons/fa6"
-import { MdOutlineHealthAndSafety } from "react-icons/md";
-import { IconType } from "react-icons";
 import React from "react";
 import Link from "next/link";
-
-interface ICategoryItem {
-    label: string,
-    name: string,
-    icon: any,
-    href: string
-}
-
-const Categories: ICategoryItem[] = [
-    { name: "personal", label: "Personal", icon:<FiUser/>, href: "/documents" },
-    { name: "finance", label: "Finance", icon: <FaMoneyBill/>, href: "/documents" },
-    { name: "health", label: "Health", icon: <MdOutlineHealthAndSafety/>, href: "/documents" }
-]
+import { Categories, ICategoryItem } from "@/utils/categories"; 
 
 
 const CategoryItem: React.FC<Omit<ICategoryItem, 'name'>> = ({ label, icon, href }) => {
@@ -26,7 +10,7 @@ const CategoryItem: React.FC<Omit<ICategoryItem, 'name'>> = ({ label, icon, href
         <li>
             <Link className="text-center w-full h-full" href={href}>
                 <div className="w-16 h-16 flex items-center justify-center rounded-lg bg-slate-300">
-                    {icon}
+                    <>{icon}</>
                 </div>
                 <p>{label}</p>
             </Link>
