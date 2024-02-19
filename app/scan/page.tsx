460fd9ede6ea270610a9ef737e24cf28c7aa593c
fortune710 <fortune0208@yahoo.com>
2024-02-18 22:09:04 -0500

    page to scan a new document image

---
+++ app/scan/page.tsx
@@ -0,0 +1,9 @@
+export default function ScanPage() {
+    return (
+        <main>
+            <input type="file" accept="image/*" capture="environment">
+                    </input>
+
+        </main>
+    )
+}
