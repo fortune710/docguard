051aba2cb6220c05de1f3a964eab1a28502e36f5
fortune710 <fortune0208@yahoo.com>
2024-02-18 20:06:40 -0500

    back button now works

---
+++ components/back-button.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
+import { ChevronLeft } from "lucide-react"
+
+const BackButton: React.FC = () => {
+    const router = useRouter();
+    
+    return (
+        <Button 
+            variant={"ghost"} 
+            onClick={() => router.back()}
+            className="border p-1 border-slate-300 rounded-lg aspect-square"
+        >
+            <ChevronLeft className="w-5 h-5" />        
+        </Button>
+    )
+}
+
+export default BackButton;
