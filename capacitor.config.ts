add3ff3a169925e4d1d1ff1832674637e34dec3c
fortune710 <fortune0208@yahoo.com>
2024-02-18 22:03:09 -0500

    defined capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appName: "DocGuard",
+    appId: "com.docguard",
+    webDir: "dist"
+}
+
+export default config;
