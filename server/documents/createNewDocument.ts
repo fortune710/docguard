3dea2c2287091f611dec3c1774d2b26cde6c696f
fortune710 <fortune0208@yahoo.com>
2024-02-18 21:45:41 -0500

    enabled adding document category

--- server/documents/createNewDocument.ts
+++ server/documents/createNewDocument.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/prisma"
+import prisma from "@/lib/prisma"
 
 const createNewDocument = async (data: any) => {
     const documents = await prisma.document.create({ data })
