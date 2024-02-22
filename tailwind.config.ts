a5ef9b4a57a754bbf2c76d283dd4f910e33c0071
fortune710 <fortune0208@yahoo.com>
2024-02-21 22:54:43 -0500

    added ppmori

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        inter: 'var(--font-inter)',
+        ppmori: 'var(--font-ppmori)'
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
