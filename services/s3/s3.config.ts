97ebef74bad58dc126b32d31007794b9a2bb874e
fortune710 <fortune0208@yahoo.com>
2024-02-22 00:39:59 -0500

    changed env variable name

--- services/s3/s3.config.ts
+++ services/s3/s3.config.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        accessKeyId: process.env.AWSACCESS_KEY!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     },
     region: "us-east-1"
--- services/ses/ses.config.ts
+++ services/ses/ses.config.ts
@@ -2,7 +2,7 @@ import { SESClient } from "@aws-sdk/client-ses";
 
 const ses = new SESClient({
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        accessKeyId: process.env.AWSACCESS_KEY!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     }
 })
