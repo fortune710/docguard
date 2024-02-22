40a2351c05fdeb6bf35cab608b44e16a52436f41
fortune710 <fortune0208@yahoo.com>
2024-02-21 23:28:43 -0500

    made logo priority true

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -6,7 +6,13 @@ export default function Header() {
     return (
         <header className='px-4 md:px-9 lg:px-12 pt-3 md:pt-5'>
             <nav className='flex items-center justify-between w-full'>
-                <Image src='/docguard.png' className='h-auto' alt='DocGuard Logo' width={180} height={60}/>
+                <Image 
+                    src='/docguard.png' 
+                    className='h-auto' 
+                    alt='DocGuard Logo' 
+                    priority={true}
+                    width={180} height={60}
+                />
 
                 <MenuButton/>
             </nav>
