aa1dc370b833f0e837673c63e1324578fc62f69d
fortune710 <fortune0208@yahoo.com>
2024-02-22 00:10:28 -0500

    created animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,10 +71,25 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+
+        "slide": {
+          from: { opacity: "0", transform: "translateY(-100px)" },
+          to: { opacity: "1", transform: "translateY(0)" }
+        },
+        "reverse-slide": {
+          from: { opacity: "0", transform: "translateY(200px)" },
+          to: { opacity: "1", transform: "translateY(0)" }
+        }
+
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+
+        "slide-up-300": "slide 1s ease-out 300ms forwards",
+        "slide-up-500": "slide 1s ease-out 500ms forwards",
+        "slide-up-700": "slide 1s ease-out 700ms forwards",
+        "slide-down": "reverse-slide 1s ease-out 1.2s forwards"
       },
     },
   },
