59c9a83049ff0198e8126f54da36bdbb1ba50158
fortune710 <fortune0208@yahoo.com>
2024-04-10 19:58:23 -0400

    configured gcp for prod

--- services/gcp-storage/storage.config.ts
+++ services/gcp-storage/storage.config.ts
@@ -1,9 +1,19 @@
 import { Storage } from "@google-cloud/storage";
-import credentials from '../../docguard-service-key.json'
-const storage = new Storage({
-    keyFile: '../../docguard-service-key.json',
-    
-    
-})
+
+export const getGCPCredentials = () => {
+    // for Vercel, use environment variables
+    return process.env.GCP_PRIVATE_KEY
+      ? {
+          credentials: {
+            client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GCP_PRIVATE_KEY,
+          },
+          projectId: process.env.GCP_PROJECT_ID,
+        }
+        // for local development, use gcloud CLI
+      : { keyFile: '../../docguard-service-key.json' };
+};
+
+const storage = new Storage(getGCPCredentials())
 
 export default storage;
