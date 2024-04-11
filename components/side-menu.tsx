60c6c8e8e6541b3416c044b553e3f976f1a64c07
fortune710 <fortune0208@yahoo.com>
2024-04-11 16:22:02 -0400

    added files to route group

--- components/side-menu.tsx
+++ components/side-menu.tsx
@@ -34,21 +34,21 @@ export default function SideMenu() {
                 <div className="flex-1">
                     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
                         <Link
-                            href="#"
+                            href="/home"
                             className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                         >
                             <Home className="h-4 w-4" />
                             Home
                         </Link>
                         <Link
-                            href="#"
+                            href="/documents?filter=all"
                             className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                         >
                             <LibraryBig className="h-4 w-4" />
                             Documents
                         </Link>
                         <Link
-                            href="#"
+                            href="/scan"
                             className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
                         >
                             <ScanLine className="h-4 w-4" />
