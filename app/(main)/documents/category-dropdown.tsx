8d61e01a240158e2e9d9b4e0dd34a9432514816b
fortune710 <fortune0208@yahoo.com>
2024-04-11 19:51:33 -0400

    made components responsive

--- app/(main)/documents/category-dropdown.tsx
+++ app/(main)/documents/category-dropdown.tsx
@@ -1,49 +1,87 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Categories } from "@/utils/categories";
+import { Filter, ListFilter } from "lucide-react";
 import { FaChevronDown } from "react-icons/fa6";
 import { StringParam, useQueryParam, withDefault } from "use-query-params";
 
 
 const CategoryDropdown: React.FC = () => {
     const [category, setCategory] = useQueryParam<string>('filter', withDefault(StringParam, 'all'));
+    const isMobile = useMediaQuery('(max-width: 640px)');
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <div className="flex items-center gap-2">
-                    <h1 className='capitalize'>
-                        { category === 'all' ? category + ' documents' : category }
-                    </h1>
-                    <FaChevronDown/>
+        <>
+            {
+                !isMobile  ? 
+                <div className="flex items-center justify-between w-full mb-4">
+                    <Tabs defaultValue="all" className="w-[400px]">
+                        <div className="flex items-center">
+                            <TabsList>
+                                <TabsTrigger onClick={() => setCategory('all')} value="all">
+                                    All
+                                </TabsTrigger>
+                                {
+                                    Categories.map(({ name, label }) => (
+                                        <TabsTrigger onClick={() => setCategory(name)} key={name} value={name}>
+                                            {label}
+                                        </TabsTrigger>
+                                    ))
+                                }
+                            </TabsList>
+                        </div>
+                    </Tabs>
+                    
+                    <div>
+                        <Button className="bg-white shadow-sm text-black border border-muted">
+                            <ListFilter/>
+                            Filter
+                        </Button>
+                    </div>
                 </div>
-            </DropdownMenuTrigger>
+                :
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <div className="flex items-center gap-2">
+                            <h1 className='capitalize'>
+                                { category === 'all' ? category + ' documents' : category }
+                            </h1>
+                            <FaChevronDown/>
+                        </div>
+                    </DropdownMenuTrigger>
 
-            <DropdownMenuContent>
-                <DropdownMenuLabel>
-                    Categories
-                </DropdownMenuLabel>
-                
-                <DropdownMenuCheckboxItem 
-                    checked={category === 'all'} 
-                    onClick={() => setCategory('all')}
-                >
-                    All Documents
-                </DropdownMenuCheckboxItem>
-                {
-                    Categories.map(({ name, label }) => (
-                    <DropdownMenuCheckboxItem
-                        key={name}
-                        onClick={() => setCategory(name)}
-                        checked={category === name}
-                    >
-                        { label }
-                    </DropdownMenuCheckboxItem>
-                    ))
-                }
-            </DropdownMenuContent>
-        </DropdownMenu>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>
+                            Categories
+                        </DropdownMenuLabel>
+                        
+                        <DropdownMenuCheckboxItem 
+                            checked={category === 'all'} 
+                            onClick={() => setCategory('all')}
+                        >
+                            All Documents
+                        </DropdownMenuCheckboxItem>
+                        {
+                            Categories.map(({ name, label }) => (
+                            <DropdownMenuCheckboxItem
+                                key={name}
+                                onClick={() => setCategory(name)}
+                                checked={category === name}
+                            >
+                                { label }
+                            </DropdownMenuCheckboxItem>
+                            ))
+                        }
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            }
+        
+        </>
 
     )
 }
--- app/(main)/documents/document-link.tsx
+++ app/(main)/documents/document-link.tsx
@@ -1,5 +1,6 @@
 
 import { Button } from "@/components/ui/button";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import getFile from "@/services/gcp-storage/getFile";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -16,24 +17,42 @@ export async function DocumentLink({ fileKey }: DocumentLinkProps) {
     const url = await getFile(fileKey);
 
     return (
-        <div className="flex items-center justify-between w-full">
-            <Link href={url} target="_blank">
-                <Button 
-                    variant='ghost'
-                    className="font-semibold" 
-                    //onClick={openDocument}
-                >
-                    View Document
+        <>
+            <div className="flex items-center justify-between w-full md:hidden">
+                <Link href={url} target="_blank">
+                    <Button 
+                        variant='ghost'
+                        className="font-semibold" 
+                        //onClick={openDocument}
+                    >
+                        View Document
+                    </Button>
+                </Link>
+
+                <Button variant={'ghost'} disabled>
+                    Get QR Code
+                    <span className="bg-zinc-900 ml-2 text-xs text-white py-0.5 px-1 rounded-xl font-semibold">
+                        Coming Soon
+                    </span>
                 </Button>
-            </Link>
+            </div>
 
-            <Button variant={'ghost'} disabled>
-                Get QR Code
-                <span className="bg-zinc-900 ml-2 text-xs text-white py-0.5 px-1 rounded-xl font-semibold">
-                    Coming Soon
-                </span>
-            </Button>
+            <div className="max-sm:hidden">
+                <DropdownMenuItem>
+                    <Link href={url}>
+                        View Document
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem disabled>
+                    Get QR Code
+                    <span className="bg-zinc-900 ml-2 text-xs text-white py-0.5 px-1 rounded-xl font-semibold">
+                        Coming Soon
+                    </span>
+                </DropdownMenuItem>
 
-        </div>
+
+            </div>
+        
+        </>
     )
 }
