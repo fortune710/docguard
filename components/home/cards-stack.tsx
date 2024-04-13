54ba368b6782e22c1b88117d3f858c13721aeedb
fortune710 <fortune0208@yahoo.com>
2024-04-12 23:56:22 -0400

    cards stack

---
+++ components/home/cards-stack.tsx
@@ -0,0 +1,9 @@
+export default function CardsStack() {
+    return (
+    <div className="stack">
+        <div className="grid w-32 h-20 rounded bg-primary text-primary-content place-content-center">1</div> 
+        <div className="grid w-32 h-20 rounded bg-accent text-accent-content place-content-center">2</div> 
+        <div className="grid w-32 h-20 rounded bg-secondary text-secondary-content place-content-center">3</div>
+    </div>    
+    )
+}
