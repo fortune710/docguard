527e270473899fb5e8bb2c6944d9a99613c0942f
fortune710 <fortune0208@yahoo.com>
2024-04-16 12:47:15 -0400

    added media queryf or responsiveness

--- components/home/new-card-button.tsx
+++ components/home/new-card-button.tsx
@@ -2,25 +2,46 @@
 
 import FileUploadDrawer from "./file-upload-drawer";
 import { Button } from "@/components/ui/button";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Plus } from "lucide-react";
 import { StringParam, useQueryParam, withDefault } from "use-query-params";
 
 
 export default function NewCardButton({ userId }: { userId: string }) {
 
-    const [_, setIsCard] = useQueryParam('is_card', withDefault(StringParam, null, true))
+    const [_, setIsCard] = useQueryParam('is_card', withDefault(StringParam, null, true));
+    const isMobile = useMediaQuery('(max-width: 768px)');
     
     return (
         <FileUploadDrawer
             userId={userId}
             uploadFileButton={
-                <Button 
-                    className="flex items-center justify-center h-[150px] w-full rounded-lg border border-dashed"
-                    variant='ghost'
-                    onClick={() => setIsCard('true')}
-                >
-                    <Plus/>
-                </Button>
+                <>
+                    {
+                        !isMobile ? 
+                        <Button 
+                            size="sm" 
+                            variant="outline"
+                            className="h-8 gap-1"
+                            onClick={() => setIsCard('true')}
+
+                        >
+                            <Plus className="h-3.5 w-3.5" />
+                            <span className="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
+                                Add New
+                            </span>
+                        </Button>                        
+                        :
+                        <Button 
+                            className="flex items-center justify-center h-[150px] w-full rounded-lg border border-dashed"
+                            variant='ghost'
+                            onClick={() => setIsCard('true')}
+                        >
+                            <Plus/>
+                        </Button>
+                        
+                    }
+                </>
             }
         />
     )
