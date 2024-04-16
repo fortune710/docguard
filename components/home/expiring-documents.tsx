aede7d117c44f0aa127af4fd5fb5edd76a33de05
fortune710 <fortune0208@yahoo.com>
2024-04-16 11:40:57 -0400

    created function to check expring components

---
+++ components/home/expiring-documents.tsx
@@ -0,0 +1,43 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter, CardContent } from "@/components/ui/card";
+import findDocumentsExpiringSoon from "@/server/documents/findDocumentsExpringSoon";
+import findExpiredDocumets from "@/server/documents/findExpiredDcouments";
+
+
+
+export default async function ExpringDocuments() {
+    const [expiredDocuments, documentsExpiringSoon] = await Promise.all([
+       await findExpiredDocumets(),
+       await findDocumentsExpiringSoon()
+    ])
+
+
+    return (
+        <>
+        
+            <Card x-chunk="dashboard-05-chunk-1">
+                <CardHeader className="pb-2">
+                    <CardDescription>Expired Documents</CardDescription>
+                    <CardTitle className="text-4xl">{expiredDocuments.length}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-xs text-muted-foreground">
+                        All clear here!
+                    </div>
+                </CardContent>
+            </Card>
+
+            <Card x-chunk="dashboard-05-chunk-2">
+                <CardHeader className="pb-2">
+                    <CardDescription>Expiring Soon</CardDescription>
+                    <CardTitle className="text-4xl">{documentsExpiringSoon.length}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-xs text-muted-foreground">
+                        Time to start renewing
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+
+    )
+}
