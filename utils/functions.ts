d9b5e2c637e5a77d4429244f45a50c8b8824d0c4
fortune710 <fortune0208@yahoo.com>
2024-04-16 12:46:27 -0400

    extracted capture image function

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,5 @@
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+
 export function generateRandomId(length: number): string {
     let id = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -7,4 +9,41 @@ export function generateRandomId(length: number): string {
     }
 
     return id;
+}
+
+export async function captureImage() {
+    const photo = await Camera.getPhoto({
+        source: CameraSource.Camera,
+        quality: 100,
+        allowEditing: true,
+        resultType: CameraResultType.DataUrl
+    })
+    
+    const parts = photo.dataUrl!.split(',');
+    //const mime = parts[0].split(':')[1].split(';')[0];
+    const data = parts[1];
+
+    // Decode the base64 data
+    const byteCharacters = atob(data);
+    
+    // Create a new ArrayBuffer and a new Uint8Array
+    const arrayBuffer = new ArrayBuffer(byteCharacters.length);
+    const uint8Array = new Uint8Array(arrayBuffer);
+
+    // Loop over the data and populate the Uint8Array with the byte values
+    for (let i = 0; i < byteCharacters.length; i++) {
+        uint8Array[i] = byteCharacters.charCodeAt(i);
+    }
+
+    // Create a new Blob object from the ArrayBuffer
+    const blob = new Blob([arrayBuffer], { type: 'image/png' });
+
+    const fileName = generateRandomId(10);
+    // Create a new File object from the Blob
+    const file = new File([blob], `${fileName}.png`, { type: 'image/png' });
+
+    return {
+        imageDataString: photo.dataUrl!,
+        actualFile: file,
+    }
 }
