53a21679c9ce60d396c38e0540bebb949214f9af
fortune710 <fortune0208@yahoo.com>
2024-05-07 21:14:03 -0400

    converted to cleint component and fetched url with useEffect

--- components/card/card-document.tsx
+++ components/card/card-document.tsx
@@ -1,10 +1,48 @@
-import getFile from "@/services/gcp-storage/getFile";
+"use client"
+import { useEffect, useMemo, useState } from "react";
 import { Card, CardContent } from "../ui/card";
 
 
 
-export default async function CardDocument({ cardSideKey }: { cardSideKey: string }) {
-    const url = await getFile(cardSideKey);
+export default function CardDocument({ cardSideKey }: { cardSideKey: string }) {
+    //const url = await getFile(cardSideKey);
+
+    const [url, setUrl] = useState<string>("");
+
+    const _ = useMemo(() => {
+        if(!cardSideKey) return;
+
+        async function getUrl() {
+            const response = await fetch(`/api/card?card_key=${cardSideKey}`, {
+                next: {
+                    tags: [cardSideKey],
+                    revalidate: 5 * 60 * 1000,
+                }
+            });
+            const { data } = await response.json()
+            setUrl(data.url);
+        }
+        getUrl()
+    }, [cardSideKey])
+    
+    /*
+    useEffect(() => {
+        if(!cardSideKey) return;
+
+        console.log(cardSideKey)
+        async function getUrl() {
+            const response = await fetch(`/api/card/${cardSideKey}`, {
+                next: {
+                    tags: [cardSideKey],
+                    revalidate: 5 * 60 * 1000,
+                }
+            });
+            const { data } = await response.json()
+            setUrl(data.url);
+        }
+        getUrl()
+    }, [cardSideKey])*/
+
 
     if(!url) {
         return (
