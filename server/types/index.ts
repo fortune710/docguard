4554b10ffbdb047967b193158546867af7ec8f8c
fortune710 <fortune0208@yahoo.com>
2024-05-09 20:59:33 -0400

    added new types

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,4 +1,6 @@
 import findUsersDocuments  from "@/server/documents/findUsersDocuments"
+import findUsersCards from "@/server/card/findUsersCards"
 
 export type Documents = Awaited<ReturnType<typeof findUsersDocuments>> 
+export type Cards = Awaited<ReturnType<typeof findUsersCards>>
 
