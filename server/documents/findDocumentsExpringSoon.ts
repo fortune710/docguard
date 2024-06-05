365d18a5062c20423b0fd4322bb44d26086a1da7
fortune710 <fortune0208@yahoo.com>
2024-06-05 19:01:58 -0400

    fixed bug with expiring documents

--- server/documents/findDocumentsExpringSoon.ts
+++ server/documents/findDocumentsExpringSoon.ts
@@ -1,18 +1,18 @@
 import prisma from "@/lib/prisma";
 import { cache } from "react";
 
-const findDocumentsExpiringSoon = cache(async () => {
+const findDocumentsExpiringSoon = cache(async (userId: string) => {
   const now = new Date();
   const sixMonthsFromNow = new Date(now.setMonth(now.getMonth() + 6));
 
   const expiringSoonDocuments = await prisma.document.findMany({
     where: {
+      user_id: userId,
       expiry_date: {
         lt: sixMonthsFromNow,
       },
     },
   });
-  console.log(expiringSoonDocuments);
   return expiringSoonDocuments;
 });
 
