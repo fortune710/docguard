d8227bff8e926a453dc4f5daa3732bd8362795fa
fortune710 <fortune0208@yahoo.com>
2024-06-07 22:30:13 -0400

    added verification modal

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -5,18 +5,28 @@ import { Input } from "@/components/ui/input";
 
 import { signIn } from "next-auth/react";
 import Link from "next/link";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
 import { FaGoogle } from "react-icons/fa6";
+import { sendVerificationEmail } from "@/services/emails/sendVerificationEmail";
+import VerifyUser from "./verify-user";
+import { useSearchParams } from "next/navigation";
 
 interface AuthFormProps {
   type: "login" | "sign-up" | "forgot-password";
 }
 
+const defaultVerificationState = {
+  open: false,
+  email: ""
+}
+
 const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
   const { toast } = useToast();
-
+  const [showVerification, setShowVerification] = useState(defaultVerificationState);
+  const searchParams = useSearchParams();
+  
   const onSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -24,7 +34,8 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
 
     switch (type) {
       case "login":
-        signIn("credentials", { email, password, callbackUrl: "/home" });
+        await signIn("credentials", { email, password, callbackUrl: "/home" });
+
         break;
       case "sign-up":
         await fetch("/api/users", {
@@ -32,6 +43,8 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
           body: JSON.stringify({ name, password, email }),
           cache: "no-store",
         });
+        setShowVerification({ open: true, email: email.toString() })
+
         toast({
           title: "Successful Sign Up",
           description: "Check your email to verify your account",
@@ -47,72 +60,87 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
 
   if (type === "login") {
     return (
-      <form
-        className="flex flex-col items-center gap-7"
-        onSubmit={onSubmitHandler}
-      >
-        <div className="flex flex-col w-full gap-5">
-          <div>
-            <label>Email</label>
-            <Input
-              placeholder="name@example.com"
-              type="email"
-              name="email"
-              className="h-11"
-            />
-          </div>
-
-          <div>
-            <label>Password</label>
-            <Input type="password" name="password" className="h-11" />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-between w-full">
-          <Link href="/forgot-password">Forgot Password</Link>
-        </div>
-
-        <Button className="w-full text-lg h-12">Login</Button>
-
-        <Button
-          variant="outline"
-          className="w-full text-lg h-12 gap-3"
-          onClick={() => signIn("google", { callbackUrl: "/home" })}
-          type="button"
+      <>
+        <form
+          className="flex flex-col items-center gap-7"
+          onSubmit={onSubmitHandler}
         >
-          <FaGoogle />
-          Sign In With Google
-        </Button>
-      </form>
+          <div className="flex flex-col w-full gap-5">
+            <div>
+              <label>Email</label>
+              <Input
+                placeholder="name@example.com"
+                type="email"
+                name="email"
+                className="h-11"
+              />
+            </div>
+
+            <div>
+              <label>Password</label>
+              <Input type="password" name="password" className="h-11" />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between w-full">
+            <Link href="/forgot-password">Forgot Password</Link>
+          </div>
+
+          <Button className="w-full text-lg h-12">Login</Button>
+
+          <Button
+            variant="outline"
+            className="w-full text-lg h-12 gap-3"
+            onClick={() => signIn("google", { callbackUrl: "/home" })}
+            type="button"
+          >
+            <FaGoogle />
+            Sign In With Google
+          </Button>
+        </form>
+
+        <VerifyUser
+          open={!!searchParams.get('unauthorized')}
+          userEmail={searchParams.get('email') ?? ''}
+        />
+      
+      </>
     );
   } else if (type === "sign-up") {
     return (
-      <form
-        className="flex flex-col items-center gap-7"
-        onSubmit={onSubmitHandler}
-      >
-        <div className="flex flex-col w-full gap-5">
-          <div>
-            <label>Name</label>
-            <Input placeholder="John Doe" name="name" className="h-11" />
+      <>
+        <form
+          className="flex flex-col items-center gap-7"
+          onSubmit={onSubmitHandler}
+        >
+          <div className="flex flex-col w-full gap-5">
+            <div>
+              <label>Name</label>
+              <Input placeholder="John Doe" name="name" className="h-11" />
+            </div>
+            <div>
+              <label>Email</label>
+              <Input
+                placeholder="name@example.com"
+                type="email"
+                name="email"
+                className="h-11"
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <Input type="password" name="password" className="h-11" />
+            </div>
           </div>
-          <div>
-            <label>Email</label>
-            <Input
-              placeholder="name@example.com"
-              type="email"
-              name="email"
-              className="h-11"
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <Input type="password" name="password" className="h-11" />
-          </div>
-        </div>
 
-        <Button className="w-full text-lg h-12">Sign Up</Button>
-      </form>
+          <Button className="w-full text-lg h-12">Sign Up</Button>
+        </form>
+      
+        <VerifyUser
+          open={showVerification.open}
+          userEmail={showVerification.email}
+        />
+      </>
     );
   }
 };
