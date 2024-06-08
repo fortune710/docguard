983b83251164db339c4f32ef241bdecf0ab9e3fe
fortune710 <fortune0208@yahoo.com>
2024-06-07 22:28:14 -0400

    send verification email on sign up

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,8 @@
 import { prisma } from "@/prisma";
 import createUser from "@/server/users/createUser";
+import { createVerification } from "@/server/verification/createVerification";
+import { sendVerificationEmail } from "@/services/emails/sendVerificationEmail";
+import { generateOTP } from "@/utils/functions";
 import { type NextRequest } from "next/server"
 
 interface User {
@@ -11,7 +14,13 @@ interface User {
 export async function POST(req: NextRequest) {
     const userData = await req.json() as User;
 
-    const newUser = await createUser(userData)
+    const newUser = await createUser(userData);
+    const otp = generateOTP();
+    
+    await Promise.all([
+        await createVerification(newUser.id, otp),
+        await sendVerificationEmail(newUser?.email!, otp)
+    ])
 
     return Response.json({
         data: newUser
