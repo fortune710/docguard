e4f901617f7a67f018aa1571a11bdb489c83cb0b
fortune710 <fortune0208@yahoo.com>
2024-06-08 01:02:33 -0400

    new type

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,6 +1,8 @@
 import findUsersDocuments  from "@/server/documents/findUsersDocuments"
 import findUsersCards from "@/server/card/findUsersCards"
+import getUserWithEmail from "@/server/users/getUserWithEmail"
 
 export type Documents = Awaited<ReturnType<typeof findUsersDocuments>> 
 export type Cards = Awaited<ReturnType<typeof findUsersCards>>
+export type User = Awaited<ReturnType<typeof getUserWithEmail>>
 
