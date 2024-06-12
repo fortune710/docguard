edc0c95118b0edeb835b4a224f82bc883bee3440
fortune710 <fortune0208@yahoo.com>
2024-06-12 12:11:20 -0400

    reduced textsize on mobile

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -12,7 +12,7 @@ export default async function HomePage() {
             <div className="flex items-center justify-between w-full">
                 <div className="my-2 md:my-4">
                     <h4 className="font-medium text-xl">Welcome Back</h4>
-                    <h1 className="font-semibold text-3xl">{user?.name!}</h1>
+                    <h1 className="font-semibold text-2xl md:text-3xl">{user?.name!}</h1>
                 </div>
 
                 <div className="md:hidden">
